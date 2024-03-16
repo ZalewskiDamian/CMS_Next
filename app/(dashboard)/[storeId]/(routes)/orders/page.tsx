059b5739e22a20c0fc841434b10a6a1b2a60d8b2
ZalewskiDamian <damian.github@gmail.com>
2024-03-16 16:18:08 +0100

    create orders nav, component

--- app/(dashboard)/[storeId]/(routes)/orders/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/orders/page.tsx
@@ -2,8 +2,8 @@ import { format } from "date-fns";
 
 import prismadb from "@/lib/prismadb";
 
-import { BillboardColumn } from "./components/columns";
-import { BillboardClient } from "./components/client";
+import { OrderColumn } from "./components/columns";
+import { OrderClient } from "./components/client";
 import { formatter } from "@/lib/utils";
 
 const OrdersPage = async ({
@@ -18,30 +18,35 @@ const OrdersPage = async ({
     include: {
       orderItems: {
         include: {
-          product: true
-        }
-      }
+          product: true,
+        },
+      },
     },
     orderBy: {
       createdAt: "desc",
     },
   });
 
-  const formattedOrders: BillboardColumn[] = orders.map((item) => ({
+  const formattedOrders: OrderColumn[] = orders.map((item) => ({
     id: item.id,
     phone: item.phone,
     adress: item.adress,
-    products: item.orderItems.map((orderItem) => orderItem.product.name).join(', '),
-    totalPrice: formatter.format(item.orderItems.reduce((total, sum) => {
-      return total + Number(item.product.price)
-    }, 0))
+    products: item.orderItems
+      .map((orderItem) => orderItem.product.name)
+      .join(", "),
+    totalPrice: formatter.format(
+      item.orderItems.reduce((total, item) => {
+        return total + Number(item.product.price);
+      }, 0)
+    ),
+    isPaid: item.isPaid,
     createdAt: format(item.createdAt, "MMMM do, yyyy"),
   }));
 
   return (
     <div className="flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6">
-        <BillboardClient data={formattedOrders} />
+        <OrderClient data={formattedOrders} />
       </div>
     </div>
   );
