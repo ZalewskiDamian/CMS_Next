335fcd14c0c7e65abbc06dad05adf1e87327fa6d
ZalewskiDamian <damian.github@gmail.com>
2024-04-06 15:51:54 +0200

    create admin dashpage overview

---
+++ components/overview.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import {
+  Bar,
+  BarChart,
+  ResponsiveContainer,
+  XAxis,
+  YAxis,
+} from "recharts";
+
+interface OverviewProps {
+  data: any[];
+}
+
+export const Overview: React.FC<OverviewProps> = ({ data }) => {
+  // Override console.error
+  // This is a hack to suppress the warning about missing defaultProps in recharts library as of version 2.12
+  // @link https://github.com/recharts/recharts/issues/3615
+  const error = console.error;
+  console.error = (...args: any) => {
+    if (/defaultProps/.test(args[0])) return;
+    error(...args);
+  };
+  return (
+    <ResponsiveContainer width="100%" height={350}>
+      <BarChart data={data}>
+        <XAxis
+          dataKey="name"
+          stroke="#888888"
+          fontSize={12}
+          tickLine={false}
+          axisLine={false}
+        />
+        <YAxis
+          stroke="#888888"
+          fontSize={12}
+          tickLine={false}
+          axisLine={false}
+          tickFormatter={(value) => `$${value}`}
+        />
+        <Bar dataKey="total" fill="#3498db" radius={[4, 4, 4, 0]} />
+      </BarChart>
+    </ResponsiveContainer>
+  );
+};
